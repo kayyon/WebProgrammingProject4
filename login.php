74562be5ab133106b6dcbe335be34c3763d2b47b
kayyon <89946346+kayyon@users.noreply.github.com>
2022-05-01 14:54:03 -0400

    added registration

---
+++ login.php
@@ -0,0 +1,38 @@
+<!-- load session variables -->
+<?php
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Properties</title>
+</head>
+
+<body>
+    <!-- include db for connection -->
+    <?php
+    include_once 'db.php';
+    ?>
+
+    <!-- WORK IN PROGRESS -->
+    <?php
+    // if (isset($_POST['submit'])) {
+    //     $first_name = $_POST['first_name'];
+    //     $last_name = $_POST['last_name'];
+    //     $username = $_POST['username'];
+    //     $email = $_POST['email'];
+    //     $password = $_POST['password'];
+    //     $confirm_password = $_POST['confirm_password'];
+    // }
+
+    // $sql_query = "INSERT INTO user (first_name, last_name, username, email, password) VALUES ('$first_name', '$last_name', '$username', '$email', '$password');";
+
+    // if (mysqli_query($)
+    ?>
+</body>
+
+</html>
